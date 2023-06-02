bbf8736155b6e0843165ad241a16a74c95f22e4c
iamsherlockholmes1999 <yerriswamytalari155@gmail.com>
2023-06-02 13:53:38 +0530

    GUVI

---
+++ server/mongo.js
@@ -0,0 +1,33 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb://0.0.0.0:27017/react-login-tut")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:false
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    conformpassword:{
+        type:String,
+        required:false
+    }
+    
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
